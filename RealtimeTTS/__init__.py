514a0f88b8e8511668c60769dd630645676cb7b6
DFJ <40487414+developerfromjokela@users.noreply.github.com>
2024-04-19 22:19:51 +0300

    Update __init__.py

--- RealtimeTTS/__init__.py
+++ RealtimeTTS/__init__.py
@@ -1,7 +1,6 @@
 from .text_to_stream import TextToAudioStream  # noqa: F401
 from .engines import BaseEngine  # noqa: F401
 from .engines import SystemEngine, SystemVoice  # noqa: F401
-from .engines import AzureEngine, AzureVoice  # noqa: F401
 from .engines import ElevenlabsEngine, ElevenlabsVoice  # noqa: F401
 from .engines import CoquiEngine, CoquiVoice  # noqa: F401
 from .engines import OpenAIEngine, OpenAIVoice  # noqa: F401
